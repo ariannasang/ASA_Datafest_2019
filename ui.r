72f2dcc8b2f24f39b8252dea59962920e18a25c5
FREDERICK CORPUZ <fcorpuz@wesleyan.edu>
2020-04-24 19:45:57 -0400

    Convert to Shinydashboard layout

--- ui.r
+++ ui.r
@@ -1,28 +1,75 @@
 library(shiny)
-library(datasets)
+library(shinydashboard)
 
-fluidPage(
-  # theme = "bootstrap_darkly.css",
-  titlePanel("Rugby7 Player Analysis"),
-  selectInput("psel", "Player", unique(phys_pca$PlayerID), selected = 1),
-  checkboxInput("limit21", "Limit to 3 weeks before Tournament?", value = FALSE),
-  splitLayout(
-    verticalLayout(
-      selectInput("tsel1", "Tournament", tdates$Tournament, selected = tdates$Tournament[1]),
-      splitLayout(
-        timeSeriesUI("physPCA1"),
-        timeSeriesUI("mentPCA1")
-      ),
-      trainingTimeUI("t1")
-      
+single_column <- function(pnum){
+  column(
+    width = 6, 
+    box(
+      title = "Player Status",
+      width = NULL, solidHeader = TRUE, status = "primary",
+      collapsible = TRUE,
+      timeSeriesUI(paste0("physPCA", pnum)), 
+      timeSeriesUI(paste0("mentPCA", pnum))
     ),
-    verticalLayout(
-      selectInput("tsel2", "Tournament", tdates$Tournament, selected = tdates$Tournament[2]),
-      splitLayout(
-        timeSeriesUI("physPCA2"),
-        timeSeriesUI("mentPCA2")
-      ),
-      trainingTimeUI("t2")
+    box(
+      title = "Training Status",
+      width = NULL, solidHeader = TRUE, status = "warning",
+      collapsible = TRUE,
+      trainingTimeUI(paste0("t", pnum))
+    ),
+  )
+  
+}
+
+header <- dashboardHeader(
+  title = "Rugby7",
+  titleWidth = 150
+)
+
+sidebar <-   dashboardSidebar(
+  width = 150,
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green"),
+    menuItem("Source code", icon = icon("file-code-o"), 
+             href = "https://github.com/ariannasang/ASA_Datafest_2019")
+  )
+)
+
+sels <- box(
+  width = 8, 
+  title = "Selections",
+  solidHeader = TRUE, status = "success", 
+  box(
+    width = 6,
+    selectInput(
+      "psel", "Player", unique(phys_pca$PlayerID), 
+      selected = sample(unique(phys_pca$PlayerID), 1)
+    ),
+    checkboxInput("limit21", "Only 3 weeks before matches?", value = FALSE)
+  ),
+  box(
+    selectInput("tsel1", "Tournament 1:", tdates$Tournament, 
+      selected = sample(tdates$Tournament, 1)
+    ),
+    selectInput("tsel2", "Tournament 2:", tdates$Tournament, 
+                selected = sample(tdates$Tournament, 1)
     )
   )
 )
+
+col1 <- single_column(1)
+col2 <- single_column(2)
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+      fluidRow(sels),
+      fluidRow(col1, col2)
+    ),
+    tabItem(tabName = "widgets",
+      h2("Widgets tab content")
+    )
+  )
+)
+dashboardPage(skin = "blue", header, sidebar, body)
